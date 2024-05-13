abc18c8ba8f2f40858ec3639be1efee5e29fe6e9
Bryan Siegel <bryansiegel@gmail.com>
2024-05-13 15:57:55 -0700

    initial commit

---
+++ src/main/java/com/bryansiegel/springcrudvaadin/SpringCrudVaadinApplication.java
@@ -0,0 +1,13 @@
+package com.bryansiegel.springcrudvaadin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudVaadinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCrudVaadinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bryansiegel/springcrudvaadin/SpringCrudVaadinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bryansiegel.springcrudvaadin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudVaadinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
